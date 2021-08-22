394697c226db15a4a7959eb4c680d36f4d935f77
HaoLiHaiO <pinelwebdevelopment@gmail.com>
2021-08-22 12:53:56 +0300

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import LoremIpsum from './LoremIpsum';
+import NotFound from './NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <Router>
+    <Switch>
+      <Route exact path="/" component={LoremIpsum} />
+      <Route component={NotFound} />
+    </Switch>
+  </Router>
   );
 }
 
